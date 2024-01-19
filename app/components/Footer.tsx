79a629fe0e083d7b4406807c00064992a6591be9
Jonathan Silva <jonalves.silva@jsmail.com.br>
2024-01-19 18:03:58 -0300

    terminar endereco

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,55 @@
+import { horarios, enderecos } from "../constants/horarios";
+
 export function Footer() {
   return (
-    <section className="flex bg-orange-500/50 h-[300px]">
-      <div></div>
-    </section>
+    <>
+      <section
+        style={{ backgroundImage: "url('assets/bg3.jpg')" }}
+        className="flex bg-cover bg-center bg-no-repeat h-full py-12 justify-center items-center min-w-[375px]"
+      >
+        <div className="flex max-w-screen-xl mx-auto bg-black h-full flex-col lg:flex-row w-[80%] gap-y-12">
+          <div className="flex flex-col items-center justify-center">
+            <div className="w-32">
+              <img src="assets/logonobg.png" className="w-full h-full"></img>
+            </div>
+            <p className="text-gray-200 text-center px-2">
+              Venha para a Gentleman e sinta a experiência de como é ser um
+              verdadeiro cavalheiro. Entre para o clube!
+            </p>
+          </div>
+          <div className="flex flex-col w-[60%] mx-auto">
+            <h3 className="text-white border-b-2 border-orange-500">
+              Horários
+            </h3>
+            {horarios.map((data) => {
+              return (
+                <div className="flex flex-row justify-between">
+                  <h3 className="text-white">{data.semana}</h3>
+                  <h3 className="text-white">{data.horario}</h3>
+                </div>
+              );
+            })}
+          </div>
+          <div className="w-[90%] mx-auto">
+            <div>
+              <h3 className="text-white border-b-2 border-orange-500">
+                Endereços
+              </h3>
+            </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-y-3 pt-2">
+              {enderecos.map((data) => {
+                return (
+                  <div className="flex p-2 justify-between">
+                    <h3 className="text-white">{data.name}</h3>
+                    <h3 className="text-white">{data.endereco}</h3>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className="bg-black h-16"></div>
+    </>
   );
 }
---
+++ app/constants/horarios.ts
@@ -0,0 +1,39 @@
+export const horarios = [
+  {
+    semana: "SEG",
+    horario: "13:00 - 20:00",
+  },
+  {
+    semana: "TER",
+    horario: "09:00 - 20:00",
+  },
+  {
+    semana: "QUA",
+    horario: "09:00 - 20:00",
+  },
+  {
+    semana: "QUI",
+    horario: "09:00 - 20:00",
+  },
+  {
+    semana: "SEX",
+    horario: "09:00 - 20:00",
+  },
+  {
+    semana: "SAB",
+    horario: "10:00 - 17:00",
+  },
+  {
+    semana: "DOM",
+    horario: "11:00 - 16:00",
+  },
+];
+
+export const enderecos = [
+  { name: "São Gonçalo", endereco: "Rua das Flores, 123 - Primavera" },
+  { name: "Niteroi", endereco: "Avenida do Sol, 456 - Vista Linda" },
+  { name: "Caxias", endereco: "Travessa da Lua, 789 - Estrelado" },
+  { name: "Rio de Janeiro", endereco: "Praça da Magia, 321 - Encantado" },
+  { name: "Nilopólis", endereco: "Alameda das Fábulas, 567 - Flores" },
+  { name: "Nova Iguaçu", endereco: "Rodovia da Imaginação, 890 - Laranjal" },
+];
